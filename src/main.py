dbac119791c3ae2d0e3baf67febcce7600f5b848
Hung Le <hunglmuit@gmail.com>
2021-03-03 16:03:31 +0700

    Update current dev
    Signed-off-by: Hung Le <hunglmuit@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
+import csv
 
-class HelloWorldExample:
+class Seeker:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -8,20 +9,62 @@ class HelloWorldExample:
     def close(self):
         self.driver.close()
 
-    def execute_greeting(self, ):
-        with self.driver.session() as session:
-            greeting = session.write_transaction(
-                self._create_one_seeker, _sid, _name, _email, _phone)
+
+    
 
     @staticmethod
+    def get_seeker(tx):
+        seekers = []
+        result = tx.run("MATCH (n:Seeker) RETURN n._id as ID, n.sid as SID")
+        for record in result:
+            seekers.append({'id':record["ID"], 'sid': record["SID"]})
+        return seekers
+
+    def get_results(self):
+        with self.driver.session() as session:
+            seekers = session.read_transaction(self.get_seeker)
+            for seeker in seekers:
+                print(seeker)
+
+
+    def merge_seeker(self, _sid, _name, _email, _phone):
+        with self.driver.session() as session:
+            session.read_transaction(self._create_one_seeker, _sid, _name, _email, _phone)
+            
+    @staticmethod
     def _create_one_seeker(tx, _sid, _name, _email, _phone):
-        result = tx.run("CREATE (n:Seeker) "
-                        "SET a.sid = $sid "
-                        "RETURN a.message + ', from node ' + id(a)", sid=_sid)
-        return result.single()[0]
+
+        tx.run("MERGE (a:Seeker {sid:$sid, name:$name, email: $email, phone:$phone}) "
+                "ON CREATE SET a._id = id(a); ", sid=_sid, name=_name, email=_email, phone=_phone)
+
+        tx.run("MATCH (a:Seeker), (b:Seeker) WHERE (a.email = b.email OR a.phone = b.phone) "
+                "and a.sid = $sid and b.sid <> $sid "
+                "SET a._id = b._id "
+                "MERGE (a)-[r:link]->(b) ", sid=_sid)
+
+
+def exec_with_csv_data(executor:Seeker):
+    with open('/mnt/d/Projects/github/graph_ql_challenge/Input/sample_input.csv') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_data:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                print(f'{row[0]}, {row[1]}, {row[2]}, {row[3]}')
+                executor.merge_seeker(row[0],row[1],row[2],row[3])
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+
 
 
 if __name__ == "__main__":
-    greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "dominic")
-    greeter.print_greeting("hello, world")
-    greeter.close()
+    executor = Seeker("bolt://localhost:7687", "neo4j", "dominic")
+
+    exec_with_csv_data(executor)
+
+    # executor.get_results()
+    
+
+    executor.close()
